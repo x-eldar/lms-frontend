df59005c434a6c4d4213b4e319a6942ef9e4cbcc
Eldar Kadymov <kadymov.01@mail.ru>
2025-06-01 21:25:48 +0300

    +

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-
-const ALLOWED_HOST = import.meta.env.VITE_ALLOWED_HOST;
-
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    allowedHosts: [
-      ALLOWED_HOST || 'localhost'
-    ]
-  }
-})
+export default defineConfig(({ mode }) => {
+  // Загружаем env-переменные для текущего режима (development/production)
+  const env = loadEnv(mode, process.cwd(), ['VITE_']);
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      host: '0.0.0.0',
+      port: 3000,
+      allowedHosts: [
+        env.VITE_ALLOWED_HOST || 'localhost'
+      ]
+    }
+  };
+});
